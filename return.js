a40835b465079156c043b427fb8e653fd7701fd0
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-07 20:39:40 +0600

    added reutur file

---
+++ return.js
@@ -0,0 +1,27 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+const output = tenTimes(10);
+// console.log("Output is : ", output);
+
+
+function square(number) {
+    const result = number * number;
+    return result;
+}
+const borgo = square(20);
+// console.log("Output is : ", borgo);
+
+// more than return 
+
+function sum (num1,num2){
+    return num1+num2;
+}
+const result= sum(20,20);
+// console.log(result);
+
+function multi (a,b){
+    return a*b;
+}
+console.log(multi(10,10));
